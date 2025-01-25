54a8ead286e3459db7d27e369f264ef82ee76b42
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-01-25 23:23:52 +0600

    db connct updated

--- api/config/db.js
+++ api/config/db.js
@@ -1,20 +1,22 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
 
 let client;
 
 const connectToDatabase = async () => {
     try {
-        if (!client || !client.isConnected()) {
-            client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        if (!client) {
+            client = new MongoClient(uri);
+            await client.connect();
+            console.log("Connected to MongoDB successfully!");
         }
+        // Optional: Check connection health
+        await client.db().admin().ping();
         return client;
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error("Error connecting to MongoDB:", error.message);
         throw error;
     }
 };
